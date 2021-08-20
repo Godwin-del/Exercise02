5527b05962db532f9b124613de6d734f2fc547ad
Godwin <Godwin.kipkirui@icealion.com>
2021-08-20 18:08:23 +0300

    exercise01 solution

---
+++ Exercise01/Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using Exercise01;
+using System;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine("Enter a Number to convert to currency");
+            string number = Console.ReadLine();
+            number = class1.TranslateNumberToWords(number);
+            Console.WriteLine(number);
+        
+            }
+    }
+}
+
